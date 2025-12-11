3882424cd3d9816043a9033fb57c8e1dbc09c4a4
harsanyilevente <levente.harsanyi@greehill.com>
2025-12-11 20:36:39 +0100

    Cleanup `llm.py`: remove commented-out code, minor formatting updates, and refine datetime handling in prompts.

--- triumvirlabs/app/llm.py
+++ triumvirlabs/app/llm.py
@@ -4,38 +4,31 @@ from dotenv import load_dotenv
 
 # Parameters
 model_type = "claude-3-haiku-20240307"  # "claude-3-5-sonnet-20241022", "claude-3-haiku-20240307" # Choose from here: https://docs.anthropic.com/en/docs/about-claude/models
-# num_examples = 5
-api_call_sleep_sec = 70  # Wait to respect API call limits
+api_call_sleep_sec = 70
 max_output_tokens = 1_000
 max_input_tokens = 20_000
 temperature = 0
 question = "Has the patient ever smoked tobacco?"
 
 # Setup
-load_dotenv()  # take environment variables from .env
+load_dotenv()
 client = anthropic.Anthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
 
 # Import data
 f = open('app/input_ehr.txt', 'r')
 ehr_medical_history_example = f.read()
-# breakpoint()
-
 
 def llm_call(question: str) -> str:
-
     prompt = f"""
     You are a helpful medical assistant that answers questions using text from the reference medical history included below.
     Be sure to respond in a complete sentence, being comprehensive, including all relevant background information.
     Strike a friendly and converstional tone. If the medical history is irrelevant to the answer, you may ignore it. 
-    In case of a timestamp just use the date without the exact time.
+    In case of timestamps or date-times just use the day without the exact time.
 
     QUESTION: {question}
     MEDICAL HISTORY: {ehr_medical_history_example}
     """
 
-    # breakpoint()
-
-    # LLM call - Single example
     message = client.messages.create(
         model=model_type,
         max_tokens=max_output_tokens,
@@ -44,6 +37,7 @@ def llm_call(question: str) -> str:
     You are a helpful medical assistant that answers questions using text from the reference medical history included below.
     Be sure to respond in a complete sentence, being comprehensive, including all relevant background information.
     strike a friendly and converstional tone. If the medical history is irrelevant to the answer, you may ignore it.
+    In case of timestamps or date-times just use the day without the exact time.
     """,
         messages=[
             {
@@ -59,11 +53,8 @@ def llm_call(question: str) -> str:
     )
 
     generated_answer = message.content[0].text
-
     return generated_answer
 
 
 if __name__ == "__main__":
-    print(question)
     generated_answer = llm_call(question=question)
-    print(generated_answer)
